eecbebd0ddebc58f24be61383fab56638a471c14
Abu Aamir <124289209+Aamir-sf@users.noreply.github.com>
2025-12-16 19:55:54 +0530

    Changed model to gemini-1.5-flash

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -85,7 +85,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-1.5-flash",
   systemInstruction: `
     AI System Instruction: Senior Code Reviewer (7+ Years of Experience)
     
