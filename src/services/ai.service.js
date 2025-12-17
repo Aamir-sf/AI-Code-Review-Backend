f7755d76c87ef955b7f436647b65c2975c546df9
Abu Aamir <124289209+Aamir-sf@users.noreply.github.com>
2025-12-17 13:05:27 +0530

    Update ai.service.js

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -87,7 +87,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash",
   systemInstruction: `
     AI System Instruction: Senior Code Reviewer (7+ Years of Experience)
     Role: Analyze code and provide a review in JSON format.
